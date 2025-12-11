a2aa5025afa9f17921d0935ea860563196441544
Mayankjg <24bmiitmscit005@gmail.com>
2025-12-11 15:21:40 +0530

    commit

--- app/ActivityHistoryPage/email/page.js
+++ app/ActivityHistoryPage/email/page.js
@@ -376,7 +376,6 @@ export default function EmailSection() {
               )}
             </div>
           </div>
-
         </div>
       </div>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+      transpilePackages: ['react-quill'],
 };
 
 export default nextConfig;
