0b08a7a2b0604f251855c5ec27faf91640d1afb1
Mayankjg <24bmiitmscit005@gmail.com>
2025-12-18 17:35:42 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   const router = useRouter();
   
   useEffect(() => {
-    router.push('/Manageitem/categories');
+    router.push('/Manageitem/lead-source');
   }, [router]);
 
   return (
